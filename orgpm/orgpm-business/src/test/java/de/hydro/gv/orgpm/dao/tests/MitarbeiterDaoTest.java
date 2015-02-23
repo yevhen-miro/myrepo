df11ffb9b7f200b7ff24b0e6847524f1cbd107e9
Yevhen Miroshnychenko <eugenemir@gmail.com>
2015-02-24 00:04:33 +0100

    orgpm-business changed tests

--- orgpm/orgpm-business/src/test/java/de/hydro/gv/orgpm/dao/tests/MitarbeiterDaoTest.java
+++ orgpm/orgpm-business/src/test/java/de/hydro/gv/orgpm/dao/tests/MitarbeiterDaoTest.java
@@ -1,5 +1,8 @@
 package de.hydro.gv.orgpm.dao.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.junit.Assert;
@@ -28,7 +31,8 @@ public class MitarbeiterDaoTest {
 				.addPackage(Logger.class.getPackage())
 				.addPackage(Mitarbeiter.class.getPackage())
 				.addAsResource("META-INF/persistence.xml",
-						"META-INF/persistence.xml").addAsManifestResource(EmptyAsset.INSTANCE,"beans.xml");
+						"META-INF/persistence.xml")
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 	}
 
 	@Inject
@@ -37,7 +41,7 @@ public class MitarbeiterDaoTest {
 	private Mitarbeiter createTestMitarbeiter() {
 		Mitarbeiter mitarbeiter = new Mitarbeiter();
 		mitarbeiter.setVorname("Yevhen");
-		mitarbeiter.setName("Miro");
+		mitarbeiter.setName("Miroshnychenko");
 		mitarbeiter.setArbeitszeit(8.0);
 		mitarbeiter.setBemerkung("Test bemerkung");
 		mitarbeiter.setgruppe("FAS");
@@ -70,4 +74,71 @@ public class MitarbeiterDaoTest {
 
 	}
 
+	@Test
+	public void testReadAllMitarbeiter() {
+
+		for (int pos = 0; pos < 3; pos++)
+			mitarbeiterDao.createMitarbeiter(new Mitarbeiter());
+		Assert.assertEquals(3, mitarbeiterDao.readAllMitarbeiter().size());
+
+	}
+
+	@Test
+	public void testUpdateMitarbeiter() {
+		Mitarbeiter mitarbeiter = createTestMitarbeiter();
+		mitarbeiterDao.createMitarbeiter(mitarbeiter);
+		mitarbeiter.setVorname("Yevheniy");
+		mitarbeiterDao.updateMitarbeiter(mitarbeiter);
+		Assert.assertEquals("Yevheniy", mitarbeiterDao
+				.readAllMitarbeiter().get(0).getVorname());
+
+	}
+
+	@Test
+	public void testDeleteMitarbeiter() {
+		Mitarbeiter mitarbeiter = createTestMitarbeiter();
+
+		mitarbeiterDao.createMitarbeiter(mitarbeiter);
+
+		mitarbeiterDao.deleteMitarbeiter(mitarbeiter);
+		Assert.assertEquals(0, mitarbeiterDao.readAllMitarbeiter().size());
+
+	}
+
+	@Test
+	public void testGetQuery() {
+		Mitarbeiter mitarbeiter = createTestMitarbeiter();
+		mitarbeiterDao.createMitarbeiter(mitarbeiter);
+		mitarbeiterDao.executeQueryWithResults("mitarbeiter.find.all");
+		Assert.assertEquals(1, mitarbeiterDao.readAllMitarbeiter().size());
+	}
+
+	@Test
+	public void readAndConvertMitarbeiter() {
+		@SuppressWarnings("unchecked")
+		List<de.hydro.gv.orgpm.Entities.Mitarbeiter> results = (List<de.hydro.gv.orgpm.Entities.Mitarbeiter>) mitarbeiterDao
+				.executeQueryWithResults("mitarbeiter.find.all");
+		ArrayList<Mitarbeiter> arrayList = new ArrayList<Mitarbeiter>();
+		for (de.hydro.gv.orgpm.Entities.Mitarbeiter mitarbeiter : results)
+			arrayList.add(convToModel(mitarbeiter));
+	}
+
+	private Mitarbeiter convToModel(
+			de.hydro.gv.orgpm.Entities.Mitarbeiter mitarbeiter) {
+		Mitarbeiter mitarbeiterModel = new Mitarbeiter();
+		mitarbeiterModel.setName(mitarbeiter.getName());
+		mitarbeiterModel.setVorname(mitarbeiter.getVorname());
+		mitarbeiterModel.setArbeitszeit(mitarbeiter.getArbeitszeit());
+		mitarbeiterModel.setBemerkung(mitarbeiter.getBemerkung());
+		mitarbeiterModel.setgruppe(mitarbeiter.getgruppe());
+		mitarbeiterModel.setHydroId(mitarbeiter.getHydroId());
+		mitarbeiterModel.setKartenNum(mitarbeiter.getKartenNum());
+		mitarbeiterModel.setMitarbeiterkennung(mitarbeiter
+				.getMitarbeiterkennung());
+		mitarbeiterModel.setMitarbeiterStatus(mitarbeiter
+				.getMitarbeiterStatus());
+		mitarbeiterModel.setPersonalNum(mitarbeiter.getPersonalNum());
+
+		return mitarbeiterModel;
+	}
 }
